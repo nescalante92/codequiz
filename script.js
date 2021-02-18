7a578b92bb417ad9dfc9f06ad756cd4457ce46e8
Nelson Escalante <nescalante92@gmail.com>
2021-02-18 01:14:18 -0500

    updated

---
+++ script.js
@@ -0,0 +1,85 @@
+//Starts Game and selects select button.
+var startBtn = document.querySelector("#start");
+
+//Add variables to represent the questions
+var questionOne = [
+{ 
+    question: "Who is Marvel's biggest Creator",
+    a: "Peter Parker" === false,
+    b: "Stan Lee" === true,
+    c: "Bruce Banner" === false,
+    d: "Bruce Wayne" === false,
+
+},
+]
+//------------------------------------------------
+var questionTwo = [
+{
+    question: "How much does Iron-Man love his daughter?(I love you...)",
+    a: "1000" === false,
+    b: "2000" === false,
+    c: "3000" === true,
+    c: "4000" === false,
+}
+]
+//------------------------------------------------
+var questionThree = [
+{
+    question: "Who's nick-name is Merc with a Mouth?",
+    a: "Spider-Man" === false,
+    b: "Deadpool" === true,
+    c: "Wolverine" === false,
+    d: "Hulk" === false,
+}
+]
+//--------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// startBtn.addEventListener("click",startGame);
