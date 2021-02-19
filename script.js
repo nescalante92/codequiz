320b4c0767fca39f6f7db675e135c264f1b7fb00
Nelson Escalante <nescalante92@gmail.com>
2021-02-18 23:55:25 -0500

    final push

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
-//Starts Game and selects select button.
-var startBtn = document.querySelector("#start");
+var startBtn = document.getElementById("startBtn");
+var timer = document.getElementById("timer");
+var questionCon = document.getElementById("questionCon");
+var questionEl = document.getElementById("question");
+var answerBtn = document.getElementById("anserBtn");
 
-//Add variables to represent the questions
-var questionOne = [
-{ 
-    question: "Who is Marvel's biggest Creator",
-    a: "Peter Parker" === false,
-    b: "Stan Lee" === true,
-    c: "Bruce Banner" === false,
-    d: "Bruce Wayne" === false,
 
-},
-]
-//------------------------------------------------
-var questionTwo = [
-{
-    question: "How much does Iron-Man love his daughter?(I love you...)",
-    a: "1000" === false,
-    b: "2000" === false,
-    c: "3000" === true,
-    c: "4000" === false,
+// this function starts the game
+function startGame () {
+    questionEl= quizQuestions
+    var quizQuestions = [
+        {
+            question: "Who is Marvel's biggest creator?",
+            a: "Peter Parker" === false,
+            b: "Stan Lee" === true,
+            c: "Bruce Banner" === false,
+            d: "Bruce Wayne" === false,
+        },
+        // ----------Add If statements here
+        {
+            question: "How much does Iron-Man love his daughter?",
+            a: "1000"=== false,
+            b: "2000"=== false,
+            c: "3000"=== true,
+            d: "4000"=== false,
+        },// ----------Add If statements here
+        {   question: "Who's nick-name is Merc with a Mouth?",
+            a:"Spider-man"=== false,
+            b:"Deadpool"=== true,
+            c:"Wolverine"=== false,
+            d:"Hulk"=== false,
+        },
+        {// ----------Add If statements here
+            question: "Where is Scarlet Witch originally from?",
+            a:"Russia"=== false,
+            b:"Slovenia"=== false,
+            c:"Czech Republic"=== false,
+            d:"Sokovia"=== true,
+        },
+        {// ----------Add If statements here
+            question: "Who is the most famous Marvel Character?",
+            a:"Captain America"=== false,
+            b:"Iron-Man"=== false,
+            c:"Superman"=== false,
+            d:"Spider-man"=== true,
+        },
+    ];
 }
-]
-//------------------------------------------------
-var questionThree = [
-{
-    question: "Who's nick-name is Merc with a Mouth?",
-    a: "Spider-Man" === false,
-    b: "Deadpool" === true,
-    c: "Wolverine" === false,
-    d: "Hulk" === false,
+
+//This function displays the questions
+function showQuestion () {
+    selectedQuestion = quizQuestion [Math.floor(Math.random() * quizQuestion.length)];
+    correctAnswers = true;
+    if (answerBtn.correct) {
+        console.log("correct")
+    }
+    
+
 }
-]
-//--------------------------------------------------
+//This function selects the answers
+function selectAnswer () {
+   
+        
+}
 
 
 
@@ -61,25 +90,3 @@ var questionThree = [
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// startBtn.addEventListener("click",startGame);
