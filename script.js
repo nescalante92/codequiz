8ce6e1dfc97f6944ef6872e4986b02939eeb1adc
Nelson Escalante <nescalante92@gmail.com>
2021-02-24 23:28:07 -0500

    updated questions and buttons

--- script.js
+++ script.js
@@ -3,65 +3,112 @@ var timer = document.getElementById("timer");
 var questionCon = document.getElementById("questionCon");
 var questionEl = document.getElementById("question");
 var answerBtn = document.getElementById("anserBtn");
-
-
+var btnOne = document.getElementById("btnOne");
+var btnTwo = document.getElementById("btnTwo");
+var btnThree = document.getElementById("btnThree");
+var btnFour = document.getElementById("btnFour");
+var count = 0;
+var quizQuestions = [
+    // // ----------Add If statements here
+    {
+        question: "Who is Marvel's biggest creator?",
+        answers: [ // quizQuestions[1].answers
+            "Peter Parker",
+            "Stan Lee",
+            "Bruce Banner",
+            "Bruce Wayne",
+        ],
+        correct: "Stan Lee"
+    },
+    {
+        question: "How much does Iron-Man love his daughter?",
+        answers: [ // quizQuestions[3].answers
+            "1000",
+            "2000",
+            "3000",
+            "4000",
+        ],
+        correct: "3000"
+    },
+    {
+        question: "Who's nick-name is Merc with a Mouth?",
+        answers: [ // quizQuestions[1].answers
+            "Spider-man",
+            "Deadpool",
+            "Wolverine",
+            "Hulk",
+        ],
+        correct: "Deadpool"
+    },
+    {
+        question: "Where is Scarlet Witch originally from?",
+        answers: [ // quizQuestions[3].answers
+            "Russia",
+            "Slovenia",
+            "Czech Republic",
+            "Sokovia",
+        ],
+        correct: "Sokovia"
+    },
+    {
+        question: "Who is the most famous Marvel Character?",
+        answers: [ //quizQuestions[3].answers
+            "Captain America",
+            "Iron-Man",
+            "Superman",
+            "Spider-man",
+        ],
+        correct: "Spider-man"
+    },
+];
 // this function starts the game
-function startGame () {
-    questionEl= quizQuestions
-    var quizQuestions = [
-        {
-            question: "Who is Marvel's biggest creator?",
-            a: "Peter Parker" === false,
-            b: "Stan Lee" === true,
-            c: "Bruce Banner" === false,
-            d: "Bruce Wayne" === false,
-        },
-        // ----------Add If statements here
-        {
-            question: "How much does Iron-Man love his daughter?",
-            a: "1000"=== false,
-            b: "2000"=== false,
-            c: "3000"=== true,
-            d: "4000"=== false,
-        },// ----------Add If statements here
-        {   question: "Who's nick-name is Merc with a Mouth?",
-            a:"Spider-man"=== false,
-            b:"Deadpool"=== true,
-            c:"Wolverine"=== false,
-            d:"Hulk"=== false,
-        },
-        {// ----------Add If statements here
-            question: "Where is Scarlet Witch originally from?",
-            a:"Russia"=== false,
-            b:"Slovenia"=== false,
-            c:"Czech Republic"=== false,
-            d:"Sokovia"=== true,
-        },
-        {// ----------Add If statements here
-            question: "Who is the most famous Marvel Character?",
-            a:"Captain America"=== false,
-            b:"Iron-Man"=== false,
-            c:"Superman"=== false,
-            d:"Spider-man"=== true,
-        },
-    ];
+function startGame() {
+    showQuestion();
+}
+startBtn.addEventListener("click", startGame);
+
+
+
+// This function displays the questions
+function showQuestion() {
+    questionEl.textContent = quizQuestions[count].question;
+    console.log(quizQuestions[count].answers[0]);
+    document.getElementById("btnOne").textContent = quizQuestions[count].answers[0];
+    console.log(quizQuestions[count].answers[1]);
+    document.getElementById("btnTwo").textContent = quizQuestions[count].answers[1];
+    console.log(quizQuestions[count].answers[2]);
+    document.getElementById("btnThree").textContent = quizQuestions[count].answers[2];
+    console.log(quizQuestions[count].answers[3]);
+    document.getElementById("btnFour").textContent = quizQuestions[count].answers[3];  
 }
 
-//This function displays the questions
-function showQuestion () {
-    selectedQuestion = quizQuestion [Math.floor(Math.random() * quizQuestion.length)];
-    correctAnswers = true;
-    if (answerBtn.correct) {
-        console.log("correct")
+function increaseCount() {
+        count++
+    if (count >= quizQuestions.length) {
+        console.log("endgame") // incude end function
+        endGame()
+    } else {
+    showQuestion();
     }
+}
+
+btnOne.addEventListener("click",increaseCount);
+btnTwo.addEventListener("click",increaseCount);
+btnThree.addEventListener("click",increaseCount);
+btnFour.addEventListener("click",increaseCount);
+    
+    
+function endGame() {
+// incude end function
+}
+
+function checkAnswers () {
+    var correctAnswer = quizQuestions[count].correct;
+    //add if statement here
+}
+
     
 
-}
-//This function selects the answers
-function selectAnswer () {
-   
-        
-}
 
 
 
@@ -84,9 +131,25 @@ function selectAnswer () {
 
 
 
+// if (answerBtn.correct) {
+    //     console.log("correct")
+    // }
 
 
+// }
+// //This function selects the answers
+// function selectAnswer () {
+// }
 
+// //-----------local storage example------------------------------------
+// var itemsEl = document.getElementById("items");
 
-
-
+// var itemsStored = JSON.parse(localStorage.getItem("iteams")) || [];
+// if (itemsEl) {
+//     for (var i = 0; i < itemsEl.length; i ++) {
+//         var item = items[i];
+//         var liEl = document.createElement('li');
+//         liEl.textContent = item;
+//         itemsEl.appendChild(liEl);
+//     }
+// }
